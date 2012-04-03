147d9c0dd2d42a47edcdeaa4b395d489121db62b
Justin Too <doubleotoo@gmail.com>
2012-04-03 09:38:30 -0700

    Add new Rails 3.1.1 project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LdapAuthentication::Application.config.secret_token = '7a9e4b86822d1dcc082f7f3ae601a741a82ab5d6efe6a98e8b4df157390d8adcc6e9f11f35af224beb1c3c52b066771bc82c87de4bb6ebaa7f6dac7268079dfc'
